55230a108378c5f12000d34cac1a56120444602f
danieldumycz <daniel.dumycz@outlook.com>
2018-03-27 16:26:48 +1100

    Basic layers added to map

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -5,25 +5,34 @@ import ol from 'ol';
 import Map from 'ol/map';
 import View from 'ol/view';
 import Tile from 'ol/layer/tile';
-import Source from 'ol/source/osm';
+import TileArcGISRest from 'ol/source/tilearcgisrest';
+import OSM from 'ol/source/osm';
 
 class MapComponent extends Component {
 
 	componentDidMount() {
-		var map = new Map({
-			view: new View({
-				center: [0, 0],
-				zoom: 1
+		var layers = [
+			new Tile({
+				source: new OSM
 			}),
-			layers: [
-				new Tile({
-					source: ""
+			new Tile({
+				extent: [-13884991, 2870341, -7455066, 6338219],
+				source: new TileArcGISRest({
+					url: 'https://sampleserver1.arcgisonline.com/ArcGIS/rest/services/' + 'Specialty/ESRI_StateCityHighway_USA/MapServer'
 				})
-			],
-			target: 'map'
+			})
+		];
+		var map = new Map({
+			layers: layers,
+			target: 'map',
+			view: new View({
+				center: [-10997148, 4569099],
+          		zoom: 4
+			})
 		});
 	}
 	render() {
+		
 		return (
 			<div id="map"></div>
 		)
